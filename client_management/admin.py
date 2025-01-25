6b024a80e2d71b8b29d70ed3a6d099344999a284
Aarif Safiyullah <56666357+aarifshoaib@users.noreply.github.com>
2025-01-25 10:42:52 +0400

    git static

--- client_management/admin.py
+++ client_management/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from client_management import models
 
+class companyModalAdmin(admin.ModelAdmin):
+    list_display = ['client', 'company_id']
+
 # Register your models here.
 admin.site.register(models.NewClientModel)
-admin.site.register(models.AddCompanyModel)
+admin.site.register(models.AddCompanyModel, companyModalAdmin)
